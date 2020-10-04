7278fa55f97299770a5f4c1ab86c24ec3580f951
dannymasao <42106941+dannymasao@users.noreply.github.com>
2020-10-04 13:29:39 +0900

    tic-tac-toe game finished

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -11,34 +11,19 @@ function Square(props) {
 }
 
 class Board extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			squares: Array(9).fill(null),
-		};
-	}
-
-	handleClick(i) {
-		const squares = this.state.squares.slice();
-		squares[i] = 'x';
-		this.setState({squares: squares});
-	}
 
 	renderSquare(i) {
 		return (
 			<Square
-				value={this.state.squares[i]}
-				onClick={() => this.handleClick(i)}
+				value={this.props.squares[i]}
+				onClick={() => this.props.onClick(i)}
 			/>
 		)
 	}
 
 	render() {
-		const status = 'Next player: X';
-
 		return (
 			<div>
-				<div className="status">{status}</div>
 				<div className="board-row">
 					{this.renderSquare(0)}
 					{this.renderSquare(1)}
@@ -60,15 +45,82 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      // The history array represents all board states, from the first to the last move
+      history: [{
+        squares: Array(9).fill(null),
+      }],
+      stepNumber: 0,
+      xIsNext: true,
+    }
+  }
+
+  handleClick(i) {
+    // This ensures that if we “go back in time” and then make a new move from that point, we throw away all the “future” history that would now become incorrect.
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+    // return early by ignoring a click if someone has won the game or if a Square is already filled
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      history: history.concat([{
+        squares: squares,
+      }]),
+      // After we make a new move, we need to update stepNumber
+      stepNumber: history.length,
+      // Each time a player moves, xIsNext will be flipped to determine which player goes next
+      xIsNext: !this.state.xIsNext,
+    });
+  }
+
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0,
+    });
+  }
+
 	render() {
+    const history = this.state.history;
+    // render the currently selected move according to stepNumber
+    const current = history[this.state.stepNumber];
+    const winner = calculateWinner(current.squares);
+
+    const moves = history.map((step, move) => {
+      const desc = move ?
+        'Go to move #' + move :
+        'Go to game start';
+      return (
+        <li key={move}>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      );
+    });
+
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
+
 		return (
 			<div className="game">
 				<div className="game-board">
-					<Board />
+          <Board
+            squares={current.squares}
+            onClick={(i) => this.handleClick(i)}
+          />
 				</div>
 				<div className="game-info">
-					<div>{/* status */}</div>
-					<ol>{/* TODO */}</ol>
+					<div>{status}</div>
+					<ol>{moves}</ol>
 				</div>
 			</div>
 		);
@@ -81,3 +133,24 @@ ReactDOM.render(
 	<Game />,
 	document.getElementById('root')
 );
+
+function calculateWinner(squares) {
+  // Given an array of 9 squares, this function will check for a winner and return 'X', 'O', or null as appropriate.
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
